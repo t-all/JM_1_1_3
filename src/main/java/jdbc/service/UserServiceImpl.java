02a2ea3c1491a12326c72ab9e6c04b631d585a9f
t-all <t-all@yandex.ru>
2021-10-16 23:18:16 +0300

    1

--- src/main/java/jdbc/service/UserServiceImpl.java
+++ src/main/java/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package jdbc.service;
 
-import jdbc.dao.UserDao;
 import jdbc.dao.UserDaoJDBCImpl;
 import jdbc.model.User;
 
@@ -8,29 +7,29 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        userDao.createUsersTable();
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDao.dropUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDao.saveUser(name, lastName, age);
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        userDao.removeUserById(id);
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDao.cleanUsersTable();
+        userDaoJDBC.cleanUsersTable();
     }
 }
